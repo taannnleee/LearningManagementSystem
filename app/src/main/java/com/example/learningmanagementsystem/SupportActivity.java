3e4a4208c99ba0bfbf92454cbeb4fe508170b1b4
TrungPham <trungpham160503@gmail.com>
2024-04-04 22:39:53 +0700

    done view login

--- app/src/main/java/com/example/learningmanagementsystem/SupportActivity.java
+++ app/src/main/java/com/example/learningmanagementsystem/SupportActivity.java
@@ -1,14 +1,4 @@
 package com.example.learningmanagementsystem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
-public class SupportActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_support);
-    }
+public class SupportActivity {
 }
