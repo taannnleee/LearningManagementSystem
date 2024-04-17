12f4736051ad0f89e8c088a26df1f25f7c6be7fd
TrungPham <trungpham160503@gmail.com>
2024-04-18 00:50:44 +0700

    fix click interaction admin

--- app/src/main/java/com/example/learningmanagementsystem/activity/InteractionAdminActivity.java
+++ app/src/main/java/com/example/learningmanagementsystem/activity/InteractionAdminActivity.java
@@ -4,75 +4,111 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.learningmanagementsystem.R;
 
 public class InteractionAdminActivity extends AppCompatActivity {
-    private ImageView imgVAddTeacher, imgVAddCourse, imgVConfirm, imgVPosts, imgVAnnouncements, imgVSupports;
+//    private ImageView imgVPosts, imgVAnnouncements, imgVSupports;
+    ImageView btnclass;
+    private ImageView imageViewAddTeacher, imageViewAddCourse, imageViewConfirm;
+    private ImageView imageViewPosts, imageViewAnnouncements, imageViewSupports;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interaction_admin);
-        getFormWidgets(); // Call this method first to initialize widgets
+
+        getFormWidgets();
+
         addEvent();
     }
 
     private void addEvent() {
-        imgVAddTeacher.setOnClickListener(new View.OnClickListener() {
+        imageViewAddTeacher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), RegisterTeacherActivity.class);
+                Intent intent = new Intent(InteractionAdminActivity.this, RegisterTeacherActivity.class);
                 startActivity(intent);
             }
         });
-        imgVAddCourse.setOnClickListener(new View.OnClickListener() {
+        imageViewAddCourse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CreationClassActivity.class);
+                Intent intent = new Intent(InteractionAdminActivity.this, CreationClassActivity.class);
                 startActivity(intent);
             }
         });
-        imgVConfirm.setOnClickListener(new View.OnClickListener() {
+        //Chua co Activity
+//        imageViewConfirm.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(InteractionAdminActivity.this, CreationClassActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+
+//        imageViewPosts.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(InteractionAdminActivity.this, SupportActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//        imageViewAnnouncements.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(InteractionAdminActivity.this, SupportActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+        imageViewSupports.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CreationClassActivity.class);
+                Intent intent = new Intent(InteractionAdminActivity.this, SupportActivity.class);
                 startActivity(intent);
             }
         });
-        // Nhung trang nay chua co view
-//        imgVPosts.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), CreationClassActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        imgVAnnouncements.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), CreationClassActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//        imgVSupports.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), CreationClassActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+
     }
 
     private void getFormWidgets() {
-        imgVAddTeacher = findViewById(R.id.imageViewAddTeacher);
-        imgVAddCourse = findViewById(R.id.imageViewAddCourse);
-        imgVConfirm = findViewById(R.id.imageViewConfirm);
-        imgVPosts = findViewById(R.id.imageViewPosts);
-        imgVAnnouncements = findViewById(R.id.imageViewAnnouncements);
-        imgVSupports = findViewById(R.id.imageViewSupports);
-    }
+        btnclass = findViewById(R.id.imageViewClasses);
+        imageViewAddTeacher = findViewById(R.id.imageViewAddTeacher);
+        imageViewAddCourse = findViewById(R.id.imageViewAddCourse);
+        imageViewConfirm = findViewById(R.id.imageViewConfirm);
+        imageViewSupports = findViewById(R.id.imageViewSupports);
+        imageViewPosts = findViewById(R.id.imageViewPosts);
+        imageViewAnnouncements = findViewById(R.id.imageViewAnnouncements);
 
+
+
+    }
 }
+
+//    private void addEvent() {
+//        imgVPosts.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), RegisterTeacherActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        imgVAnnouncements.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), RegisterTeacherActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        imgVSupports.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), RegisterTeacherActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+
--- app/src/main/java/com/example/learningmanagementsystem/activity/SupportActivity.java
+++ app/src/main/java/com/example/learningmanagementsystem/activity/SupportActivity.java
@@ -2,15 +2,38 @@ package com.example.learningmanagementsystem.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.learningmanagementsystem.R;
 
 public class SupportActivity extends AppCompatActivity {
+    ImageButton btn_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+        getFormWidgets(); // Call this method first to initialize widgets
+        addEvent();
+
+    }
+
+    private void addEvent() {
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SupportActivity.this, InteractionAdminActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void getFormWidgets() {
+        btn_back = findViewById(R.id.btn_back);
+
     }
 }
