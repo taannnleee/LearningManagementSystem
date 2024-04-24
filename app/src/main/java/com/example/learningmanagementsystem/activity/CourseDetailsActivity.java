17c747b659edc1b0403eca98ff2de5ca96172091
TrungPham <trungpham160503@gmail.com>
2024-04-24 12:03:36 +0700

    view class Details

---
+++ app/src/main/java/com/example/learningmanagementsystem/activity/CourseDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.example.learningmanagementsystem.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.learningmanagementsystem.R;
+
+public class CourseDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+    }
+}
