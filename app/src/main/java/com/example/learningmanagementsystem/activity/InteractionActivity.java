ad765cf6c99f397f623aff87cd4b95678e95275a
Tân <leetaan1902@gmail.com>
2024-05-02 14:47:42 +0700

    push

--- app/src/main/java/com/example/learningmanagementsystem/activity/InteractionActivity.java
+++ app/src/main/java/com/example/learningmanagementsystem/activity/InteractionActivity.java
@@ -3,17 +3,21 @@ package com.example.learningmanagementsystem.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.learningmanagementsystem.R;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 
 public class InteractionActivity extends AppCompatActivity {
     private ImageView imgVToeic, imgVIelts, imgVCommunication;
     private ImageView imgVAssignments, imgVHomework, imgVAbsence;
     private  ImageView imgVPosts, imgVAnnouncements, imgVSupports;
+    private BottomNavigationItemView action_home;
+    private BottomNavigationItemView  interaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +28,19 @@ public class InteractionActivity extends AppCompatActivity {
 
     private void addEvent() {
         // cac ham chuyen trang
+        interaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        action_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InteractionActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
         imgVToeic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,8 +74,12 @@ public class InteractionActivity extends AppCompatActivity {
         imgVToeic = findViewById(R.id.imageViewToeic);
         imgVIelts = findViewById(R.id.imageViewIelts);
         imgVCommunication = findViewById(R.id.imageViewCommunication);
+        action_home = findViewById(R.id.action_home);
+        interaction = findViewById(R.id.interaction);
 
-        //mấy cái dưới chưa dùng đến nha Trung
+
+
+        //mấy cái dưới chưa dùng đến nha Trung nhớ xóa
         imgVAssignments = findViewById(R.id.imageViewAssignments);
         imgVHomework = findViewById(R.id.imageViewHomework);
         imgVAbsence = findViewById(R.id.imageViewAbsence);
